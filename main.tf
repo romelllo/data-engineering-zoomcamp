226e82ea2ae3220afb343b7ff2275949b77572db
romelllo <romannovikov526@gmail.com>
2025-01-23 15:15:56 +0400

    add terraform

---
+++ main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.17.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "copper-diorama-448706-a3"
+  region  = "us-central1"
+}
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "copper-diorama-448706-a3_terraform-demo-bucket"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
