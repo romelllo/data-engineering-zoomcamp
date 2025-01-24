cf3b4aa67472ec35b9bf6e6c4b0464bdd274ff62
romelllo <romannovikov526@gmail.com>
2025-01-24 11:24:39 +0400

    add variables.tf file

--- main.tf
+++ main.tf
@@ -8,13 +8,14 @@ terraform {
 }
 
 provider "google" {
-  project = "copper-diorama-448706-a3"
-  region  = "us-central1"
+  credentials = file(var.credentials)
+  project     = var.project_id
+  region      = var.region
 }
 
-resource "google_storage_bucket" "demo-bucket" {
-  name          = "copper-diorama-448706-a3_terraform-demo-bucket"
-  location      = "US"
+resource "google_storage_bucket" "demo_bucket" {
+  name          = var.gcs_bucket_name
+  location      = var.locaiton
   force_destroy = true
 
   lifecycle_rule {
@@ -34,4 +35,8 @@ resource "google_storage_bucket" "demo-bucket" {
       type = "AbortIncompleteMultipartUpload"
     }
   }
+}
+
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = var.bq_dataset_name
 }
---
+++ variables.tf
@@ -0,0 +1,40 @@
+variable "credentials" {
+  description = "Service Account Credentials"
+  default     = "./keys/service-account.json"
+}
+
+variable "project_id" {
+  description = "Project ID"
+  type        = string
+  default     = "copper-diorama-448706-a3"
+}
+
+variable "region" {
+  description = "Project Region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "locaiton" {
+  description = "Project Location"
+  type        = string
+  default     = "US"
+}
+
+variable "bq_dataset_name" {
+  description = "BigQuery dataset name"
+  type        = string
+  default     = "demo_dataset"
+}
+
+variable "gcs_bucket_name" {
+  description = "Bucket Name"
+  type        = string
+  default     = "copper-diorama-448706-a3_terraform_demo_bucket"
+}
+
+variable "gcs_storage_class" {
+  description = "Bucket Storage Class"
+  type        = string
+  default     = "STANDARD"
+}
